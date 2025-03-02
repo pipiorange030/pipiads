4f6a347194e6c962c7204f6ceaebe0e6a4d60e06
pipiorange030 <147233508+pipiorange030@users.noreply.github.com>
2025-03-03 00:30:19 +0800

    Update BanJS.js

--- BanJS.js
+++ BanJS.js
@@ -1,7 +1,7 @@
 // 定义 CSS/JS
 const regex = '<head>';
 const replace_str = '<head>\
-<link rel="stylesheet" href="https://raw.githubusercontent.com/pipiorange030/pipiads/refs/heads/main/css" type="text/css">'
+<link rel="stylesheet" href="https://raw.githubusercontent.com/pipiorange030/pipiads/refs/heads/main/css.css" type="text/css">'
 
 // 定义响应体
 const body = $response.body.replace(regex, replace_str);
