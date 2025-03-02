d8cdb08379a032466cd910690dfd0ae322bc715e
pipiorange030 <147233508+pipiorange030@users.noreply.github.com>
2025-03-03 00:10:37 +0800

    Create BanJS.js

---
+++ BanJS.js
@@ -0,0 +1,9 @@
+// 定义 CSS/JS
+const regex = '<head>';
+const replace_str = '<head>\
+<link rel="stylesheet" href="https://raw.githubusercontent.com/pipiorange030/pipiads/refs/heads/main/css" type="text/css">'
+
+// 定义响应体
+const body = $response.body.replace(regex, replace_str);
+
+$done({ body: body })
